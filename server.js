bc0898afa6010ad2d218363f5d160c26fce4dc28
smdhar <shoumik_dhar@yahoo.com>
2025-07-23 13:23:07 -0400

    Remove excessive VIP route and change landing page

--- server.js
+++ server.js
@@ -38,14 +38,6 @@ app.get('/', async (req, res) => {
     });
 });
 
-app.get('/vip-lounge', (req, res) => {
-    if (req.session.user) {
-        res.send(`Welcome to the party ${req.session.user.username}`);
-    } else {
-        res.send('Sorry, no guests allowed');
-    }
-});
-
 app.use('/auth', authController);
 
 app.listen(port, () => {
