a77c366c8564b2c4099ab52911d1f19860261dcc
smdhar <shoumik_dhar@yahoo.com>
2025-07-23 15:08:19 -0400

    Build the application controller skeleton

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
+const applicationsController = require('./controllers/applications.js');
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -46,6 +47,7 @@ app.get('/', async (req, res) => {
 app.use('/auth', authController); // these are the auth routes
 app.use(isSignedIn); // we are setting this one up after the person CAN sign in
 // so that other routes can be protected
+app.use('/users/:userId/applications', applicationsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
