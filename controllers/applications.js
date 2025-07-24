1680c45f93cbfe42eed14c26db89e966a2f93ef7
smdhar <shoumik_dhar@yahoo.com>
2025-07-24 13:29:20 -0400

    Complete new application functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -17,4 +17,19 @@ router.get('/new', async (req, res) => {
     res.render('applications/new.ejs');
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+
+        currentUser.applications.push(req.body);
+
+        await currentUser.save();
+
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
