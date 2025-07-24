e01540033fc9e7daccfcf5fcb5e5b19d7742713c
smdhar <shoumik_dhar@yahoo.com>
2025-07-24 11:24:31 -0400

    Complete applications index route and view template

--- controllers/applications.js
+++ controllers/applications.js
@@ -4,5 +4,13 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // we will add our routes
+router.get('/', (req, res) => {
+    try {
+        res.render('applications/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
